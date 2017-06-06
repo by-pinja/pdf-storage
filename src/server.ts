6eb898f805c7193eaf1df10c0f1f1b9e7a7d09a5
Savpek <savolainen.pekka@gmail.com>
2017-06-06 15:44:49 +0300

    Cleanup.

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-
-const app: express.Application = express();
-const port: number = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    // Success callback
-    console.log(`Listening at http://localhost:${port}/`);
-});
