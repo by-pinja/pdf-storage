dc4f5d29b9b11966958e1cfc21476ddf806ef42b
Savpek <savolainen.pekka@gmail.com>
2017-06-22 14:50:38 +0300

    Added healthcheck controller.

---
+++ Health/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pdf.Storage.Health
+{
+    public class HealthController : Controller
+    {
+        [HttpGet("/healtz")]
+        public IActionResult HealtCheck()
+        {
+            return Ok(new { }); ;
+        }
+
+        // Google ingress requires root to return ok code.
+        [HttpGet("/")]
+        public IActionResult RootCheck()
+        {
+            return Ok(new { });
+        }
+    }
+}
