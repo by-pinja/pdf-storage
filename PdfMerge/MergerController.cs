6eb6a034f7dc2ef89623743a0931f011b60cff58
Savpek <savolainen.pekka@gmail.com>
2017-07-24 14:37:30 +0300

    Added simple validations for pdf merge.

--- PdfMerge/MergerController.cs
+++ PdfMerge/MergerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Hangfire;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -23,6 +24,12 @@ namespace Pdf.Storage.PdfMerge
         [HttpPost("v1/merge/{groupId}/")]
         public IActionResult MergePdfs(string groupId, [Required][FromBody] MergeRequest[] requests)
         {
+            if (requests.Length < 2)
+                return BadRequest();
+
+            if (!AllRequestedFilesExists(requests))
+                return BadRequest();
+
             var entity = _context.PdfFiles.Add(new PdfEntity(groupId)).Entity;
 
             var filePath = $"{_settings.BaseUrl}/v1/pdf/{groupId}/{entity.FileId}.pdf";
@@ -32,6 +39,13 @@ namespace Pdf.Storage.PdfMerge
             _backgroundJob.Enqueue<IPdfMerger>(merger => merger.MergePdf(entity.GroupId, entity.FileId, requests));
 
             return Accepted(new MergeResponse(entity.FileId, filePath));
+        }
+
+        private bool AllRequestedFilesExists(MergeRequest[] requests)
+        {
+            var groups = requests.Select(x => x.Group).Distinct();
+            var keys = _context.PdfFiles.Where(x => groups.Any(g => g == x.GroupId)).Select(x => $"{x.GroupId}_{x.FileId}");
+            return requests.All(r => keys.Any(k => k == $"{r.Group}_{r.PdfId}"));
         }
     }
 }
