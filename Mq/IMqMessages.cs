2f9200755bb29f69068c0ca1f97f49d2b57e5b31
Savpek <savolainen.pekka@gmail.com>
2017-08-29 18:30:08 +0300

    Implemented MQ messages on pdf open and generated.

---
+++ Mq/IMqMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Pdf.Storage.Mq
+{
+    public interface IMqMessages
+    {
+        void PdfGenerated(string groupId, string pdfId);
+        void PdfOpened(string groupId, string pdfId);
+    }
+}
---
+++ Mq/MqMessagesNullObject.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Pdf.Storage.Pdf;
+using Pdf.Storage.Util;
+
+namespace Pdf.Storage.Mq
+{
+    public class MqMessagesNullObject: IMqMessages
+    {
+        private readonly ILogger<MqMessagesNullObject> _logger;
+
+        public MqMessagesNullObject(ILogger<MqMessagesNullObject> logger)
+        {
+            _logger = logger;
+        }
+
+        public void PdfGenerated(string groupId, string pdfId)
+        {
+            _logger.LogInformation($"{nameof(PdfGenerated)}: Mq disabled");
+        }
+
+        public void PdfOpened(string groupId, string pdfId)
+        {
+            _logger.LogInformation($"{nameof(PdfOpened)}: Mq disabled");
+        }
+    }
+}
