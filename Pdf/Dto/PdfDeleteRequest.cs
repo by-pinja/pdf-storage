d9aaa615e2a874406141f221cc92545793124030
Savpek <savolainen.pekka@gmail.com>
2017-12-18 08:34:02 +0200

    Updated delete api.

---
+++ Pdf/Dto/PdfDeleteRequest.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pdf.Storage.Pdf.Dto
+{
+    public class PdfDeleteRequest
+    {
+        [Required]
+        public string GroupId { get; set;}
+        [Required]
+        public string PdfId { get; set;}
+    }
+}
