d397ba3bd2fc83df2faa251351997aab9539a99d
Savpek <savolainen.pekka@gmail.com>
2018-01-16 18:52:13 +0200

    Added migrations.

---
+++ Migrations/20180116164959_DeleteApi.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Pdf.Storage.Migrations
+{
+    public partial class DeleteApi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PdfOpenedCallbackUri",
+                table: "PdfFiles");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Removed",
+                table: "PdfFiles",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Removed",
+                table: "PdfFiles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PdfOpenedCallbackUri",
+                table: "PdfFiles",
+                nullable: true);
+        }
+    }
+}
