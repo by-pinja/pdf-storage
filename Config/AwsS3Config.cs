ae3b04742cbdd2c8386cf2a7d7df5a55c394a3d9
Atte Tarvainen <atte.tarvainen@protacon.com>
2018-06-01 11:17:47 +0300

    Update AwsS3Config.cs

--- Config/AwsS3Config.cs
+++ Config/AwsS3Config.cs
@@ -2,10 +2,10 @@ namespace Pdf.Storage.Config
 {
     public class AwsS3Config
     {
-        public string AwsS3BucketName = "pdf-storage-master";
-        public string AccessKey = "thisisaccesskey";
-        public string SecretKey = "ThisIsSecretKey";
-        public string AwsServiceURL = "http://localhost:900";
-        public string AwsRegion = "EUCentral1";
+        public string AwsS3BucketName { get; set; } = "pdf-storage-master";
+        public string AccessKey { get; set; } = "thisisaccesskey";
+        public string SecretKey { get; set; } = "ThisIsSecretKey";
+        public string AwsServiceURL { get; set; } = "http://localhost:900";
+        public string AwsRegion { get; set; } = "EUCentral1";
     }
 }
