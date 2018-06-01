94dfb901b6780f1c2c9e784c7595fad241698f70
Savpek <savolainen.pekka@gmail.com>
2018-06-01 13:00:24 +0300

    Committing temporary state.

--- Hangfire/HangfireQueue.cs
+++ Hangfire/HangfireQueue.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using Hangfire;
+using Hangfire.States;
 
 namespace Pdf.Storage.Hangfire
 {
@@ -22,5 +24,13 @@ namespace Pdf.Storage.Hangfire
         {
             _backgroundJobClient.Schedule(methodCall, delay);
         }
+
+        public string PrioritizeJob(string id)
+        {
+            _backgroundJobClient.Delete(id);
+            return _backgroundJobClient.Enqueue(methodCall);
+        }
+
+        public static IEnumerable<string> GetValidQueues => new string[] { "critical", "default"};
     }
 }
