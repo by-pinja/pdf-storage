b13b5caee53a2097fe4fe2a57b7da101c26e19eb
Savpek <savolainen.pekka@gmail.com>
2018-09-26 09:58:25 +0300

    Class as static.

--- Hangfire/HangfireConstants.cs
+++ Hangfire/HangfireConstants.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace Pdf.Storage.Hangfire
 {
-    public class HangfireConstants
+    public static class HangfireConstants
     {
         public const string HighPriorityQueue = "critical";
         public const string DefaultPriorityQueue = "default";
