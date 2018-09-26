4a339fc3e1c600f322584bf46eeb5f7ae273aa02
Pekka Savolainen <savolainen.pekka@gmail.com>
2018-09-26 11:54:20 +0300

    Attempt to implement retry with new hangfire feature. (#15)
    * Attempt to implement retry with new hangfire feature.
    
    * Class as static.
    
    * Small naming tweak.

--- Hangfire/HangfireConstants.cs
+++ Hangfire/HangfireConstants.cs
@@ -2,10 +2,10 @@ using System.Collections.Generic;
 
 namespace Pdf.Storage.Hangfire
 {
-    public class HangfireConstants
+    public static class HangfireConstants
     {
         public const string HighPriorityQueue = "critical";
         public const string DefaultPriorityQueue = "default";
-        public static IEnumerable<string> Enumerate() => new [] { HighPriorityQueue, DefaultPriorityQueue };
+        public static IEnumerable<string> GetQueues() => new [] { HighPriorityQueue, DefaultPriorityQueue };
     }
 }
