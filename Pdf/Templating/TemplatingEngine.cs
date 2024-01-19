e2217e93fb42ed854783bcc86b6af4c98d509035
Savpek <savolainen.pekka@gmail.com>
2024-01-19 17:17:10 +0200

    Dropped barcodes

--- Pdf/Templating/TemplatingEngine.cs
+++ Pdf/Templating/TemplatingEngine.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Text.RegularExpressions;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Pdf.Storage.Pdf.Templating;
 using Stubble.Core;
 using Stubble.Core.Builders;
 using Stubble.Extensions.JsonNet;
@@ -15,68 +8,10 @@ namespace Pdf.Storage.Pdf
     public class TemplatingEngine
     {
         private readonly StubbleVisitorRenderer _stubble = new StubbleBuilder().Configure(settings => settings.AddJsonNet()).Build();
-        private readonly BarcodeLib.Barcode _barcode = new BarcodeLib.Barcode();
-        private readonly Regex _barcodeTranslatorPattern = new Regex(@"\[translate\:barcode(\:)?(.*)?\](.*)");
 
         public string Render(string template, JObject data)
         {
-            ApplyBarcodeTranslatorsIfAny(data);
-
             return _stubble.Render(template, data);
-        }
-
-        private void ApplyBarcodeTranslatorsIfAny(JObject data)
-        {
-            foreach (var property in data.Properties())
-            {
-                var translatorMatch = _barcodeTranslatorPattern.Match(property.Value.ToString());
-
-                if (translatorMatch.Success)
-                {
-                    using var memoryStream = new MemoryStream();
-
-                    var options = GetBarcodeOptions(translatorMatch);
-
-                    var barcode = _barcode.Encode(
-                        ResolveBarcodeType(options.Type),
-                        translatorMatch.Groups[3].Value,
-                        ColorTranslator.FromHtml(options.ForegroundColor),
-                        ColorTranslator.FromHtml(options.BackgroundColor),
-                        options.Width,
-                        options.Height);
-
-                    barcode.Save(memoryStream, ImageFormat.Png);
-
-                    property.Value = $"data:image/png;base64,{Convert.ToBase64String(memoryStream.ToArray())}";
-                }
-            }
-        }
-
-        private static BarcodeOptions GetBarcodeOptions(Match translatorMatch)
-        {
-            var options = new BarcodeOptions();
-
-            if (!string.IsNullOrEmpty(translatorMatch.Groups[2].Value))
-            {
-                options = JsonConvert.DeserializeObject<BarcodeOptions>(translatorMatch.Groups[2].Value);
-            }
-
-            return options;
-        }
-
-        private BarcodeLib.TYPE ResolveBarcodeType(string type)
-        {
-            return type switch
-            {
-                "code128" => BarcodeLib.TYPE.CODE128,
-                "ean13" => BarcodeLib.TYPE.EAN13,
-                "ean8" => BarcodeLib.TYPE.EAN8,
-                "upca" => BarcodeLib.TYPE.UPCA,
-                "upce" => BarcodeLib.TYPE.UPCE,
-                "itf14" => BarcodeLib.TYPE.ITF14,
-                "code39" => BarcodeLib.TYPE.CODE39,
-                _ => BarcodeLib.TYPE.CODE128,
-            };
         }
     }
 }
--- Test/TemplatingTests.cs
+++ Test/TemplatingTests.cs
@@ -27,49 +27,5 @@ namespace Pdf.Storage.Test
                     x.Should().Match("*header_value_here*key_for_row_0*");
                 });
         }
-
-        [Fact]
-        public async Task WhenBarcodeTranslationIsApplied_ThenApplyTranslationAndUpdateTemplate()
-        {
-            var host = TestHost.Run<TestStartup>();
-            var groupId = Guid.NewGuid();
-
-            var newPdf = (await host
-                .AddPdf(groupId,
-                    html: "<img src=\"{{ barcode }}\"/>",
-                    baseData: new { barcode = "[translate:barcode]AE5C9B"}))
-                .Single();
-
-            await host.Get(newPdf.HtmlUri)
-                .WithContentOf<string>()
-                .Passing(x =>
-                {
-                    x.Should().Match("*<img src=\"data:image/png;base64,*\"/>*");
-                });
-        }
-
-        [Fact]
-        public async Task WhenBarcodeTranslationWithOptionsIsApplied_ThenApplyTranslationToTemplatesWithDefinedOptions()
-        {
-            var host = TestHost.Run<TestStartup>();
-            var groupId = Guid.NewGuid();
-
-            var newPdf = (await host
-                .AddPdf(groupId,
-                    html: "<img src=\"{{ barcode }}\"/>",
-                    baseData: new { barcode = "[translate:barcode:{includeText: true, foregroundColor: '#4286f4'}]AE5C9B"}))
-                .Single();
-
-            await host.Get(newPdf.HtmlUri)
-                .WithContentOf<string>()
-                .Passing(x =>
-                {
-                    // Note here: It's bit tricky to properly test that image matches expectation for options.
-                    // It's possible that this test will cause false positive on depency update of libraries if image generation changes:
-                    // if so manually validate image as correct one and update this test again to match expectations.
-                    // This is here to alert possible error if something have changed for image generation.
-                    x.Should().Match("*<img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUh*\"/>*");
-                });
-        }
     }
 }
